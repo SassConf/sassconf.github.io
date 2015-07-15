058a4e24dfe298303fc95142ade1215e378f5305
Elyse Holladay <elyseholladay@gmail.com>
2015-07-15 14:50:46 -0500

    days until!!1

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -3,3 +3,20 @@ function toggle(id) {
   e.classList.toggle('is-active');
   console.log('toggle' + e);
 }
+
+
+//Set the two dates
+today=new Date()
+var conference=new Date(today.getFullYear(), 10, 11) //Month is 0-11 in JavaScript
+
+// console.log(Date(today.getFullYear()));
+// console.log(conference);
+
+if (today.getMonth()==11 && today.getDate()>25) //if conference has passed already
+    conference.setFullYear(conference.getFullYear()+1) //calculate next year's conference
+//Set 1 day in milliseconds
+var one_day=1000*60*60*24
+
+// Find Days Until box and put in the correct number
+document.getElementById('daysuntil-number').innerHTML = (Math.ceil((conference.getTime()-today.getTime())/(one_day)));
+
