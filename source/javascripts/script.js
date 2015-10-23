20e5b581a99adadd074fdb967051a9666330d559
Adekunle <adekunleoduye@hotmail.com>
2015-10-23 02:20:28 -0400

    expand and collaspe for workshops

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -24,9 +24,30 @@ $(document).ready(function(){
   var $subHeader = $('.sub-header');
   var $subHeaderStripes = $('<div class="sub-header__stripes"></div>');
   var $subHeaderContainer = $('<div class="sub-header__container"></div>');
+  // var $workshopLearnMoreBtn = $('.js-workshops__learn-more');
 
   $subHeader.wrap($subHeaderContainer);
   $subHeader.after($subHeaderStripes);
+
+  // Click a element to hide
+  // var clickHide = function(el) {
+  //     $(this).addClass('hide');
+  // }
+
+
+  $('.js-workshops__learn-more').bind('click', function(e){
+    e.preventDefault();
+    $(this).addClass('hide');
+    $(this).parents('.workshop').find('.workshops__description').removeClass("workshops__description--is-hidden");
+  });
+
+  $('.js-description__close-btn').bind('click', function(e){
+    e.preventDefault();
+    $('.js-workshops__learn-more').removeClass('hide');
+    $(this).parents('workshops__description').addClass("workshops__description--is-hidden");
+
+    console.log($(this).parents('.workshops__description').addClass("workshops__description--is-hidden"));
+  })
 
   //$subHeader.append($subHeaderStripes);
 });
