c46e98e9d7d0a6deab68d77aa261ea61618aaf60
Aisha Green <agreen@signpost.com>
2015-11-08 20:02:28 -0600

    add package.json

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,23 +1,25 @@
-function toggle(id) {
+$(document).ready(function() {
+
+  function toggle(id) {
   var e = document.getElementById(id);
   e.classList.toggle('is-active');
-  console.log('toggle' + e);
-}
+  //console.log('toggle' + e);
+  }
 
 
-//Set the two dates
-today=new Date()
-var conference=new Date(today.getFullYear(), 10, 11) //Month is 0-11 in JavaScript
+  //Set the two dates
+  var today = new Date();
+  var conference = new Date(today.getFullYear(), 10, 11); //Month is 0-11 in JavaScript
 
-// console.log(Date(today.getFullYear()));
-// console.log(conference);
+  // console.log(Date(today.getFullYear()));
+  // console.log(conference);
 
-if (today.getMonth()==11 && today.getDate()>25) //if conference has passed already
-    conference.setFullYear(conference.getFullYear()+1) //calculate next year's conference
-//Set 1 day in milliseconds
-var one_day=1000*60*60*24
+  if (today.getMonth()==11 && today.getDate()>25); //if conference has passed already
+      conference.setFullYear(conference.getFullYear()+1); //calculate next year's conference
+  //Set 1 day in milliseconds
+  var one_day=1000*60*60*24;
 
-$(document).ready(function(){
+
   var $subHeader = $('.sub-header');
   var $subHeaderStripes = $('<div class="sub-header__stripes"></div>');
   var $subHeaderContainer = $('<div class="sub-header__container"></div>');
@@ -31,14 +33,13 @@ $(document).ready(function(){
   //     $(this).addClass('hide');
   // }
 
-
-  $('.js-workshops__learn-more').bind('click', function(e){
+  $('.js-workshops__learn-more').bind('click', function(e) {
     e.preventDefault();
     $(this).addClass('hide');
     $(this).parents('.workshop').find('.workshops__description').removeClass("workshops__description--is-hidden");
   });
 
-  $('.js-description__close-btn').bind('click', function(e){
+  $('.js-description__close-btn').bind('click', function(e) {
     e.preventDefault();
     $('.js-workshops__learn-more').removeClass('hide');
     $(this).parents('workshops__description').addClass("workshops__description--is-hidden");
@@ -53,9 +54,7 @@ $(document).ready(function(){
   //   return date;
   // }
 
-
+  // Find Days Until box and put in the correct number
+  document.getElementById('daysuntil-number').innerHTML = (Math.ceil((conference.getTime()-today.getTime())/(one_day)));
 
 });
-
-// Find Days Until box and put in the correct number
-document.getElementById('daysuntil-number').innerHTML = (Math.ceil((conference.getTime()-today.getTime())/(one_day)));
